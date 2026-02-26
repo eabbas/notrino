c4293430edbf8b2e7d50af57fb09ac7c0419ff1f
servin <servinnasiri45@gmail.com>
2026-02-26 16:00:52 +0330

    ..

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -21,7 +21,7 @@
       <!-- top -->
       <section class="relative z-50 max-w-[1700px] h-20 bg-white flex justify-between items-center px-1 md:px-20 shadow-xl">
         <!-- menu-mobile -->
-        <div id="hambeger" class="sticy md:absolute">
+        <div id="hambeger" class="sticy md:absolute pr-3">
           <div class="menu-mobile sticy md:absolute md:hidden" onclick="Hamberger('open',this)">
             <svg class=" sticy md:absolute md:hidden fill-zinc-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
               <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
